ef99c9e3fd02fb74e232ae12cb9490d06669dc09
Bruno Cruz <brunocruz@Brunos-MacBook-Air.local>
2024-07-09 19:17:31 +0100

    Create lumen project, create connection for DB. Create Product model and controller and update routes. At the Product controller update the functions.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //Get all data from database
+        $products = Product::all();
+
+        return response()->json($products);
+    }
+    public function store(Request $request)
+    {
+        //Post data to DB from user
+
+        //Validation
+        $this->validate($request,[
+            'title' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $product = new Product();
+
+        if($request->hasFile('photo')){
+            $file = $request->file('photo');
+            $allowfileExtension = ['jpg', 'png', 'jpeg'];
+            $extension = $file->getClientOriginalExtension();
+            $check = in_array($extension, $allowfileExtension);
+
+            if($check){
+                $name = time() . $file->getClientOriginalName();
+                $file->move('images', $name);
+                $product->photo = $name;
+            }
+        }
+
+        $product->title = $request->input('title');
+        $product->price = $request->input('price');
+        $product->description = $request->input('description');
+        $product->save();
+
+        return response()->json($product);
+    }
+
+    public function show($id)
+    {
+        //Give 1 item from products table
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //Update product by Id
+    }
+
+    public function destroy($id)
+    {
+        //Delete product by Id
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product successfully deleted!');
+    }
+}
